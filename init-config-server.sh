565e2e10441a5c6f7011e6655fd0abdb1396e7dd
octopus-kim <kk97sj@gmail.com>
2025-03-09 11:21:13 +0900

    [FEAT] /webhook 오류 수정

---
+++ init-config-server.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Jenkins의 Execute Shell에서 동작하는 코드입니다.
+
+# 명령어 실패 시 스크립트 종료
+set -euo pipefail
+
+# 로그 출력 함수
+log() {
+  echo "[$(date +'%Y-%m-%d %H:%M:%S')] $*"
+}
+
+# 에러 발생 시 로그와 함께 종료하는 함수
+error() {
+  log "Error on line $1"
+  exit 1
+}
+
+trap 'error $LINENO' ERR
+
+log "스크립트 실행 시작."
+
+# docker network 생성
+if docker network ls --format '{{.Name}}' | grep -q '^nansan-network$'; then
+  log "Docker network named 'nansan-network' is already existed."
+else
+  log "Docker network named 'nansan-network' is creating..."
+  docker network create --driver bridge nansan-network
+fi
+
+# Build Gradle
+log "build gradle"
+./gradlew clean build
+
+# 기존 config-server 이미지를 삭제하고 새로 빌드
+log "config-server image remove and build."
+docker rmi config-server:latest || true
+docker build -t config-server:latest .
+
+# Docker로 config-server 서비스 실행
+log "Execute config-server..."
+docker run -d \
+  --name config-server \
+  --restart unless-stopped \
+  -e CONFIG_SERVER_NAME=${CONFIG_SERVER_NAME} \
+  -e CONFIG_SERVER_PASSWORD=${CONFIG_SERVER_PASSWORD} \
+  -e CONFIG_SERVER_GIT_URI=${CONFIG_SERVER_GIT_URI} \
+  -e GIT_USERNAME=${GIT_USERNAME} \
+  -e GIT_PASSWORD=${GIT_PASSWORD} \
+  -e RABBITMQ_USERNAME=${RABBITMQ_USERNAME} \
+  -e RABBITMQ_PASSWORD=${RABBITMQ_PASSWORD} \
+  -p 8888:8888 \
+  -v /var/config-server:/app/data \
+  --network nansan-network \
+  config-server:latest
+
+echo "==== Succeed!!! ===="
